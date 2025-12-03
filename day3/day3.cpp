81b42c3f79473b9cb638c9ddbca7d8e3db05cdad
lamasmithueten <ticonderoga122@mgail.com>
2025-12-03 11:24:44 +0100

    Finished day 3

---
+++ day3/day3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int findBiggestNumber(std::string string, int length){
+	int biggestNumber = 0;
+	char current = string[0];
+	int index = 1;
+	for (int i =0; i<length-1; i++){
+		if (string[i]>current){
+			current = string[i];
+			index = i+1;
+		}
+	}
+	biggestNumber = (current- '0') *10;
+	current = string[index];
+	for (int i = index; i<length;i++){
+		if (string[i]>current) current = string[i];
+	}
+
+	biggestNumber += current - '0';
+	
+	return biggestNumber;
+}
+
+
+int main (int argc, char ** argv){
+	std::ifstream file (argv[1]);
+	std::string line;
+	int sum = 0;	
+
+	while (std::getline(file, line)){
+		int size = line.length();
+		sum +=findBiggestNumber(line, size);
+}
+
+	std::cout << "\n\n" << sum << '\n';
+
+	return 0;
+}
---
+++ day3/day3_part2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+unsigned long long pow (unsigned long long base, unsigned long long exponent){
+	unsigned long long result = 1;
+	for (unsigned long long i = 0; i<exponent; i++) result *= base;
+	return result;
+}
+
+unsigned long long findBiggestNumber(std::string string, unsigned long long length){
+	unsigned long long biggestNumber = 0;
+	char current = 0;
+	unsigned long long index = 0;
+
+	for (unsigned long long i = 12; i; i--){
+		current = string[index];
+		for (unsigned long long j=index; j <(length-i+1); j++){
+			if (string[j]>current){
+				current=string[j];
+				index = j;
+			}
+		}
+		biggestNumber +=(current-'0') *pow(10, i-1);
+		index++;
+	}
+
+	return biggestNumber;
+}
+
+
+int main (int argc, char ** argv){
+	std::ifstream file (argv[1]);
+	std::string line;
+	unsigned long long sum = 0;	
+
+	while (std::getline(file, line)){
+		unsigned long long size = line.length();
+		sum +=findBiggestNumber(line, size);
+}
+
+	std::cout << "\n\n" << sum << '\n';
+
+	return 0;
+}
