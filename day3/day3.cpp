ae59c5ddda712657d7ca2b1d29689502d3c21249
lamasmithueten <ticonderoga122@mgail.com>
2025-12-04 18:04:09 +0100

    changed makefile and formatting in source files

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
-int findBiggestNumber(std::string string, int length){
-	int biggestNumber = 0;
-	char current = string[0];
-	int index = 1;
-	for (int i =0; i<length-1; i++){
-		if (string[i]>current){
-			current = string[i];
-			index = i+1;
-		}
-	}
-	biggestNumber = (current- '0') *10;
-	current = string[index];
-	for (int i = index; i<length;i++){
-		if (string[i]>current) current = string[i];
-	}
+int findBiggestNumber(std::string string, int length) {
+  int biggestNumber = 0;
+  char current = string[0];
+  int index = 1;
+  for (int i = 0; i < length - 1; i++) {
+    if (string[i] > current) {
+      current = string[i];
+      index = i + 1;
+    }
+  }
+  biggestNumber = (current - '0') * 10;
+  current = string[index];
+  for (int i = index; i < length; i++) {
+    if (string[i] > current)
+      current = string[i];
+  }
 
-	biggestNumber += current - '0';
-	
-	return biggestNumber;
+  biggestNumber += current - '0';
+
+  return biggestNumber;
 }
 
+int main(int argc, char **argv) {
+  std::ifstream file(argv[1]);
+  std::string line;
+  int sum = 0;
 
-int main (int argc, char ** argv){
-	std::ifstream file (argv[1]);
-	std::string line;
-	int sum = 0;	
+  while (std::getline(file, line)) {
+    int size = line.length();
+    sum += findBiggestNumber(line, size);
+  }
 
-	while (std::getline(file, line)){
-		int size = line.length();
-		sum +=findBiggestNumber(line, size);
-}
+  std::cout << sum << '\n';
 
-	std::cout << sum << '\n';
-
-	return 0;
+  return 0;
 }
--- day4/day4_part2.cpp
+++ day4/day4_part2.cpp
@@ -1,114 +1,141 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
+bool isValid(const std::vector<std::string> &matrix, int x, int y) {
+  int count = 0;
+  int x_max = matrix.size() - 1;
+  int y_max = matrix[y].length() - 1;
 
-bool isValid(const std::vector<std::string>& matrix, int x, int y) {
-	int count = 0;
-	int x_max = matrix.size()-1;
-	int y_max = matrix[y].length()-1;
+  // Check left side
+  if (x == 0) {
+    // Check top side
+    if (y == 0) {
+      if (matrix[x + 1][y] == '@')
+        count++;
+      if (matrix[x + 1][y + 1] == '@')
+        count++;
+      if (matrix[x][y + 1] == '@')
+        count++;
+      return (count <= 3);
+    }
+    // Check bottom side
+    if (y == y_max) {
+      if (matrix[x][y - 1] == '@')
+        count++;
+      if (matrix[x + 1][y - 1] == '@')
+        count++;
+      if (matrix[x + 1][y] == '@')
+        count++;
+      return (count <= 3);
+    }
+    if (matrix[x + 1][y - 1] == '@')
+      count++;
+    if (matrix[x + 1][y] == '@')
+      count++;
+    if (matrix[x + 1][y + 1] == '@')
+      count++;
+    if (matrix[x][y - 1] == '@')
+      count++;
+    if (matrix[x][y + 1] == '@')
+      count++;
+    return (count <= 3);
+  }
 
-	//Check left side
-	if(x==0){
-		//Check top side
-		if(y==0){
-			if(matrix[x+1][y] == '@') count++;
-			if(matrix[x+1][y+1] == '@') count++;
-			if(matrix[x][y+1] == '@') count++;
-			return (count<=3);
-		}
-		//Check bottom side
-		if(y==y_max){
-			if(matrix[x][y-1] == '@') count++;
-			if(matrix[x+1][y-1] == '@') count++;
-			if(matrix[x+1][y] == '@') count++;
-			return (count<=3);
-		}
-		if(matrix[x+1][y-1] == '@') count++;
-		if(matrix[x+1][y] == '@') count++;
-		if(matrix[x+1][y+1] == '@') count++;
-		if(matrix[x][y-1] == '@') count++;
-		if(matrix[x][y+1] == '@') count++;
-		return (count<=3);
-	}
+  // Check right side
+  if (x == x_max) {
+    // Check top side
+    if (y == 0) {
+      if (matrix[x - 1][y] == '@')
+        count++;
+      if (matrix[x - 1][y + 1] == '@')
+        count++;
+      if (matrix[x][y + 1] == '@')
+        count++;
+      return (count <= 3);
+    }
+    // Check bottom side
+    if (y == y_max + 1) {
+      if (matrix[x - 1][y] == '@')
+        count++;
+      if (matrix[x - 1][y - 1] == '@')
+        count++;
+      if (matrix[x][y - 1] == '@')
+        count++;
+      return (count <= 3);
+    }
+    if (matrix[x][y - 1] == '@')
+      count++;
+    if (matrix[x][y + 1] == '@')
+      count++;
+    if (matrix[x - 1][y - 1] == '@')
+      count++;
+    if (matrix[x - 1][y] == '@')
+      count++;
+    if (matrix[x - 1][y + 1] == '@')
+      count++;
+    return (count <= 3);
+  }
 
-	//Check right side
-	if (x==x_max ){
-		//Check top side
-		if(y==0){
-			if(matrix[x-1][y] == '@') count++;
-			if(matrix[x-1][y+1] == '@') count++;
-			if(matrix[x][y+1] == '@') count++;
-			return (count<=3);
-		}
-		//Check bottom side
-		if(y==y_max+1){
-			if(matrix[x-1][y] == '@') count++;
-			if(matrix[x-1][y-1] == '@') count++;
-			if(matrix[x][y-1] == '@') count++;
-			return (count<=3);
-		}
-		if(matrix[x][y-1] == '@') count++;
-		if(matrix[x][y+1] == '@') count++;
-		if(matrix[x-1][y-1] == '@') count++;
-		if(matrix[x-1][y] == '@') count++;
-		if(matrix[x-1][y+1] == '@') count++;
-		return (count<=3);
-	}
+  if (matrix[x - 1][y - 1] == '@')
+    count++;
+  if (matrix[x - 1][y] == '@')
+    count++;
+  if (matrix[x - 1][y + 1] == '@')
+    count++;
+  if (matrix[x][y - 1] == '@')
+    count++;
+  if (matrix[x][y + 1] == '@')
+    count++;
+  if (matrix[x + 1][y - 1] == '@')
+    count++;
+  if (matrix[x + 1][y] == '@')
+    count++;
+  if (matrix[x + 1][y + 1] == '@')
+    count++;
 
-
-	if(matrix[x-1][y-1] == '@') count++;
-	if(matrix[x-1][y] == '@') count++;
-	if(matrix[x-1][y+1] == '@') count++;
-	if(matrix[x][y-1] == '@') count++;
-	if(matrix[x][y+1] == '@') count++;
-	if(matrix[x+1][y-1] == '@') count++;
-	if(matrix[x+1][y] == '@') count++;
-	if(matrix[x+1][y+1] == '@') count++;
-
-	return (count<=3);
+  return (count <= 3);
 }
 
-int main (int argc, char ** argv){
-	std::ifstream file (argv[1]);
-	std::string line;
-	std::vector <std::string> array;
-	int sum=0;
+int main(int argc, char **argv) {
+  std::ifstream file(argv[1]);
+  std::string line;
+  std::vector<std::string> array;
+  int sum = 0;
 
-	while (std::getline(file, line)){
-		array.push_back(line);
-	}
+  while (std::getline(file, line)) {
+    array.push_back(line);
+  }
 
-	int size = array.size();
-	int length = array[0].length();
-	while(1){
-		int changes=0;
-		for (int i= 0 ; i<size; i++){
-			for (int j = 0; j < length; j++){
-				if (array[i][j]=='@'){
-					if (isValid(array, i, j)){
-						array[i][j]='.';
-						sum++;
-						changes++;
-//						std::cout << 'x';
-					}
-	/*				else{
-						std::cout << '@';
-					}*/
-				}
-	/*			else{
-					std::cout << '.';
-				}*/
+  int size = array.size();
+  int length = array[0].length();
+  while (1) {
+    int changes = 0;
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < length; j++) {
+        if (array[i][j] == '@') {
+          if (isValid(array, i, j)) {
+            array[i][j] = '.';
+            sum++;
+            changes++;
+            //						std::cout << 'x';
+          }
+          /*				else{
+                                                  std::cout << '@';
+                                          }*/
+        }
+        /*			else{
+                                        std::cout << '.';
+                                }*/
+      }
+      //		std::cout << '\n';
+    }
+    if (!changes)
+      break;
+  }
 
-			}
-	//		std::cout << '\n';
-		}
-		if(!changes) break;
-	}
+  std::cout << "Sum: " << sum << '\n';
 
-
-	std::cout << "Sum: " << sum << '\n';
-
-	return 0;
+  return 0;
 }
